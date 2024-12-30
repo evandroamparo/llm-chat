c7b829b5affa2024a741b752cf1f18c133feb386
Evandro da Silva Amparo <1426462+evandroamparo@users.noreply.github.com>
2024-12-30 00:07:13 +0000

    basic chat with Open AI client and Groq API key

---
+++ ConsoleChat/Program.cs
@@ -0,0 +1,18 @@
+﻿using dotenv.net;
+using OpenAI.Chat;
+
+DotEnv.Load();
+
+var apiKey = Environment.GetEnvironmentVariable("GROQ_API_KEY") ?? "";
+
+ChatClient client = new(
+    model: "llama-3.3-70b-versatile", 
+    credential: new System.ClientModel.ApiKeyCredential(apiKey),
+    options: new OpenAI.OpenAIClientOptions
+    {
+        Endpoint = new Uri("https://api.groq.com/openai/v1")
+    });
+
+ChatCompletion completion = client.CompleteChat("tell me a joke");
+
+Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
