a5b47480e79d860e11d886ec70a8ead8920aca8b
Evandro da Silva Amparo <1426462+evandroamparo@users.noreply.github.com>
2025-01-02 02:31:23 +0000

    added main chat loop

--- ConsoleChat/Program.cs
+++ ConsoleChat/Program.cs
@@ -3,16 +3,30 @@ using OpenAI.Chat;
 
 DotEnv.Load();
 
+const string EXIT_COMMAND = "/bye";
+
 var apiKey = Environment.GetEnvironmentVariable("GROQ_API_KEY") ?? "";
 
 ChatClient client = new(
-    model: "llama-3.3-70b-versatile", 
+    model: "llama-3.3-70b-versatile",
     credential: new System.ClientModel.ApiKeyCredential(apiKey),
     options: new OpenAI.OpenAIClientOptions
     {
         Endpoint = new Uri("https://api.groq.com/openai/v1")
     });
 
-ChatCompletion completion = client.CompleteChat("tell me a joke");
+var exit = false;
+do
+{
+    Console.Write("> ");
+    var prompt = Console.ReadLine();
+    exit = string.Compare(prompt, EXIT_COMMAND, StringComparison.OrdinalIgnoreCase) == 0;
 
-Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+    if (exit)
+        continue;
+
+    ChatCompletion completion = await client.CompleteChatAsync(prompt);
+
+    Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+} while (!exit);
+
